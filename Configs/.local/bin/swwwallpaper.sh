da955893ba70cd52e7c1d852ae78f9aacda41c6c
yagodanr <yagodanr@gmail.com>
2026-04-10 16:41:54 +0300

    updated to use awww for wallpapers(swww is deprecated)

--- Configs/.local/bin/swwwallpaper.sh
+++ Configs/.local/bin/swwwallpaper.sh
@@ -15,8 +15,8 @@ Wall_Cache()
 {
     ln -fs "${wallList[setIndex]}" "${wallSet}"
     ln -fs "${wallList[setIndex]}" "${wallCur}"
-    "${scrDir}/swwwallcache.sh" -w "${wallList[setIndex]}" &> /dev/null
-    "${scrDir}/swwwallbash.sh" "${wallList[setIndex]}" &
+    "${scrDir}/awwwallcache.sh" -w "${wallList[setIndex]}" &> /dev/null
+    "${scrDir}/awwwallbash.sh" "${wallList[setIndex]}" &
     ln -fs "${thmbDir}/${wallHash[setIndex]}.sqre" "${wallSqr}"
     ln -fs "${thmbDir}/${wallHash[setIndex]}.thmb" "${wallTmb}"
     ln -fs "${thmbDir}/${wallHash[setIndex]}.blur" "${wallBlr}"
@@ -93,12 +93,12 @@ while getopts "nps:" option ; do
 done
 
 
-#// check swww daemon
+#// check awww daemon
 
-swww query &> /dev/null
+awww query &> /dev/null
 if [ $? -ne 0 ] ; then
-    swww-daemon --format xrgb &
-    swww query && swww restore
+    awww-daemon --format xrgb &
+    awww query && awww restore
 fi
 
 
@@ -112,4 +112,4 @@ fi
 #// apply wallpaper
 
 echo ":: applying wall :: \"$(readlink -f "${wallSet}")\""
-swww img "$(readlink "${wallSet}")" --transition-bezier .43,1.19,1,.4 --transition-type "${xtrans}" --transition-duration "${wallTransDuration}" --transition-fps "${wallFramerate}" --invert-y --transition-pos "$(hyprctl cursorpos | grep -E '^[0-9]' || echo "0,0")" &
+awww img "$(readlink "${wallSet}")" --transition-bezier .43,1.19,1,.4 --transition-type "${xtrans}" --transition-duration "${wallTransDuration}" --transition-fps "${wallFramerate}" --invert-y --transition-pos "$(hyprctl cursorpos | grep -E '^[0-9]' || echo "0,0")" &
